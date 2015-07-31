5095c943a2dc1844e0fe55e69a503470787b9292
shundroid <ra24483@qb3.so-net.ne.jp>
2015-07-31 14:01:48 +0900

    音楽の選択画面【面途中】

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 window.addEventListener("load", function() {
   console.log("Hello World!");
 });
+
+var select_btn = document.getElementById("btn_select");
+select_btn.addEventListener("click", () => {
+  document.getElementById("file_select").click();
+});
+document.getElementById("file_select").onchange = function() {
+	console.log(this.files);
+	document.getElementById("audio_play").src = this.value;
+	document.getElementById("audio_play").play();
+};
