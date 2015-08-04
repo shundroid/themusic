d9973a66c882e1c178e24933d2fd6bdba0ebd1f1
shundroid <ra24483@qb3.so-net.ne.jp>
2015-08-04 14:49:58 +0900

    オーディオの再生機能を実装

--- app.js
+++ app.js
@@ -4,10 +4,23 @@ window.addEventListener("load", function() {
 
 var select_btn = document.getElementById("btn_select");
 select_btn.addEventListener("click", () => {
-  document.getElementById("file_select").click();
+  //document.getElementById("file_select").click();
+  var activity = new MozActivity({
+  	name: "pick",
+  	data: {
+      type: "audio/mpeg"
+  	}
+  });
+  activity.onsuccess = function() {
+  	console.log("SUCCESS");
+  	var audioUrl = URL.createObjectURL(this.result.blob);
+  	console.log(audioUrl);
+  	var audio = new Audio(audioUrl);
+  	audio.play();
+  };
 });
-document.getElementById("file_select").onchange = function() {
-	console.log(this.files);
-	document.getElementById("audio_play").src = this.value;
-	document.getElementById("audio_play").play();
-};
+// document.getElementById("file_select").onchange = function() {
+// 	console.log(this.files);
+// 	document.getElementById("audio_play").src = this.value;
+// 	document.getElementById("audio_play").play();
+// };
